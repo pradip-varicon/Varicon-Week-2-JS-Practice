33c18814c47aa6854bb258fc0efeda044ca2a040
Pradip Singh <pradip.singh@varicon.com.au>
2024-08-15 10:26:57 +0545

    Total Sales for Each Product is done

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
 const salesData = require("./data/salesData.json");
+const calculateTotalSales = require("./utils/totalSalesByEachProducts");
 const calculateTotalRevenue = require("./utils/totalRevenue");
 
+console.log("Total Sales for Each Product: ", calculateTotalSales(salesData));
 console.log("Total Revenue: ", calculateTotalRevenue(salesData));
